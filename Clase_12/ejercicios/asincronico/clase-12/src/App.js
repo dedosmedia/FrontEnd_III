aa0f65c75b96e6b61fb6f11a4dceb2675491ddff
dedosmedia <gerencia@dedosmedia.com>
2021-08-28 21:37:14 -0500

    Clase 12. Eventos y ciclo de vida

---
+++ Clase_12/ejercicios/asincronico/clase-12/src/App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import './App.css';
+import swal from 'sweetalert2'
+
+class Contador extends React.Component {
+
+  constructor( props ) {
+    super( props );
+
+    this.state = {
+      counter:  this.props.init
+    }
+  }
+
+
+  componentDidMount() { 
+    console.log("El componente fue montado en el DOM");
+  }
+
+  componentDidUpdate(prev, post) { 
+    console.log("El componente fue actualizado");
+    console.log(prev);
+    console.log(post);
+  }
+
+
+  handleDecrement = () => {
+    if( this.state.counter <= 0 ) {
+      swal.fire("Contador no puede ser inferior a 0")
+    }
+    else {
+      this.setState( { counter: this.state.counter - 1 })
+    } 
+  }
+
+  handleIncrement = () => {
+    if( this.state.counter >= 10 ) {
+      swal.fire("Contador no puede ser superior a 10")
+    }
+    else {
+      this.setState( { counter: this.state.counter + 1 })
+    } 
+  }
+
+  render() {
+    return (
+      <>
+        <div>El conteo es: { this.state.counter }</div>
+        <button onClick={ this.handleIncrement}>+</button>
+        <button onClick={ this.handleDecrement}>-</button>
+      </>
+    )
+  }
+
+}
+
+
+function App() {
+  return (
+    <Contador init={5}/>
+  );
+}
+
+export default App;
